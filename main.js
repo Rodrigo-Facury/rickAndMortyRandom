48b9f98abad4129a8a015d66b6053f078e9e56bf
Rodrigo <rodrigo.facury14@gmail.com>
2023-06-07 21:59:11 -0300

    feat: filtra personagens recebidos pela API

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 window.onload = () => {
+  let characters = [];
+  let filteredCharacters = [];
+  const searchInput = document.getElementById('search-input');
+  
+  function renderCharacters(charactersArray) {
+    const cardList = document.getElementById('products');
+    cardList.innerHTML = '';
+
+    charactersArray.forEach((character) => {
+      const cardList = document.getElementById('products');
+      const card = document.createElement('div');
+      card.className = 'product-card';
+      const image = document.createElement('img');
+      image.setAttribute('src', character.image);
+      image.setAttribute('alt', character.name);
+      const name = document.createElement('h3');
+      name.innerText = character.name;
+      cardList.appendChild(card);
+      card.appendChild(image);
+      card.appendChild(name);
+    });
+  }
+  
+  function filterCharacters(event) {
+    const name = event.target.value;
+    filteredCharacters = characters.filter((character) => character.name
+      .toLowerCase().includes(name.toLowerCase()));
+    
+    renderCharacters(filteredCharacters);
+  }
+
+  searchInput.addEventListener('keyup', filterCharacters);
+
   fetch('https://rickandmortyapi.com/api/character')
     .then((res) => res.json())
     .then((json) => {
-      const characters = json.results;
+      characters = json.results;
 
-      characters.forEach((character) => {
-        const cardList = document.getElementById('products');
-        const card = document.createElement('div');
-        card.className = 'product-card';
-        const image = document.createElement('img');
-        image.setAttribute('src', character.image);
-        image.setAttribute('alt', character.name);
-        const name = document.createElement('h3');
-        name.innerText = character.name;
-        cardList.appendChild(card);
-        card.appendChild(image);
-        card.appendChild(name);
-      });
+      renderCharacters(characters);
     })
     .catch((err) => console.error(err))
 }
