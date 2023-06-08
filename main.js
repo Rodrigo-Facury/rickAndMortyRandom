72510df8f409b65494ce816fe23e194b4a5d1341
Rodrigo <rodrigo.facury14@gmail.com>
2023-06-07 21:02:49 -0300

    refactor: renderiza personagem aleatório utilizando async await

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.onload = async () => {
   const MAX_NUMBER = 826;
 
   const randomNumber = Math.random() * MAX_NUMBER;
@@ -8,20 +8,23 @@ window.onload = () => {
     round = 1;
   }
 
-  fetch(`https://rickandmortyapi.com/api/character/${round}`)
-    .then((res) => res.json())
-    .then((character) => {
-      const cardList = document.getElementById('products');
-      const card = document.createElement('div');
-      card.className = 'product-card';
-      const image = document.createElement('img');
-      image.setAttribute('src', character.image);
-      image.setAttribute('alt', character.name);
-      const name = document.createElement('h3');
-      name.innerText = character.name;
-      cardList.appendChild(card);
-      card.appendChild(image);
-      card.appendChild(name);
-    })
-    .catch((err) => console.error(err))
+  try {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${round}`);
+
+    const character = await res.json();
+  
+    const cardList = document.getElementById('products');
+    const card = document.createElement('div');
+    card.className = 'product-card';
+    const image = document.createElement('img');
+    image.setAttribute('src', character.image);
+    image.setAttribute('alt', character.name.split(' ')[0]);
+    const name = document.createElement('h3');
+    name.innerText = character.name.split(' ')[0];
+    cardList.appendChild(card);
+    card.appendChild(image);
+    card.appendChild(name);
+  } catch (err) {
+    console.log(err);
+  }
 }
