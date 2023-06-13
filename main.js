612d01f807f35f7fd1a0de982689c36b49c1b54a
Rodrigo <rodrigo.facury14@gmail.com>
2023-06-13 20:51:54 -0300

    feat: adiciona lógica de alterar cor dos cards e persistir informações no localstorage

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 window.onload = () => {
+  let color = '';
+
+  const classname = localStorage.getItem('classname');
+
+  if (classname) {
+    color = classname;
+  }
+
   fetch('https://rickandmortyapi.com/api/character')
     .then((res) => res.json())
     .then((json) => {
@@ -7,7 +15,7 @@ window.onload = () => {
       characters.forEach((character) => {
         const cardList = document.getElementById('products');
         const card = document.createElement('div');
-        card.className = 'product-card';
+        card.className = `product-card ${color}`;
         const image = document.createElement('img');
         image.setAttribute('src', character.image);
         image.setAttribute('alt', character.name);
@@ -19,4 +27,18 @@ window.onload = () => {
       });
     })
     .catch((err) => console.error(err))
+
+    const colorButtons = document.getElementsByClassName('color-button');
+    
+    const cards = document.getElementsByClassName('product-card');
+
+    for (let i = 0; i < colorButtons.length; i += 1) {
+      colorButtons[i].addEventListener('click', (e) => {
+        localStorage.setItem('classname', e.target.name);
+
+        for (let index = 0; index < cards.length; index += 1) {
+          cards[index].className = `product-card ${e.target.name}`;
+        }
+      })
+    }
 }
